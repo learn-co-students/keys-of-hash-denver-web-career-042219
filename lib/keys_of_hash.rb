70ab40158d5131ea3e5b55c9bb05894edad4472a
Joe Tustin <j.tustin@gmail.com>
2019-03-25 18:43:54 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    listOfAnimals = []
+
+      self.each do |animal, country|
+    #    binding.pry
+        if arguments.include?(country)
+          listOfAnimals << animal
+      end
+    end
+    return listOfAnimals
   end
 end
+
+
+#check if arguments includes the country value in the each, shovel out the animal and return the array.
