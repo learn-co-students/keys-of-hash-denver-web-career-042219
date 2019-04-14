14ef800f08cd23bff393aff396e3ebe6a4b00258
Blake A. Long <blake.a.long@gmail.com>
2019-04-13 18:44:09 -0600

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    result = []
+     arguments.each do | item |
+      self.each do | key, value |
+       if value == item
+        result << key
+       end
+     end
+    end
+   result
   end
 end
